68b18549e2e5afffdf73bd4f03bc38633d059dbd
VaidyJ <vai_j@yahoo.com>
2019-09-25 22:23:58 -0500

    Delete testNasa.py

--- VisualProject/backup/testNasa.py
+++
@@ -1,7 +0,0 @@
-ApiKey= 'eMpdHL6BBBPdurOf5a9PbX276bW4lcSgcn1Nltgo'
-
-url = 'https://api.nasa.gov/insight_weather/?api_key=eMpdHL6BBBPdurOf5a9PbX276bW4lcSgcn1Nltgo&feedtype=json&ver=1.0'
-
-response = requests.get(url).json()
-
-print(response)
